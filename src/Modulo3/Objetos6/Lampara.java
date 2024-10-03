979ffecc1a06228d0143c14ee69d3e42f4756571
Lautaro Montaña <joselautarom@gmail.com>
2024-10-03 08:23:48 -0300

    Objeto 6

---
+++ src/Modulo3/Objetos6/Lampara.java
@@ -0,0 +1,17 @@
+package Modulo3.Objetos6;
+
+public class Lampara {
+
+    boolean encendida;
+
+    // Metodo para cambiar el estado de la lampara
+    public void cambiarEstado(){
+        encendida = !encendida;
+    }
+
+    // Mostrar el estado de la lampara
+    public String estadoLampara(){
+        return encendida ? "encendida" : "apagada";
+    }
+    
+}
---
+++ src/Modulo3/Objetos6/Main.java
@@ -0,0 +1,24 @@
+package Modulo3.Objetos6;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        // Instanciamos el objeto
+        Lampara lampara1 = new Lampara();
+    
+        lampara1.encendida = false; //Inicialmente apagada
+
+        JOptionPane.showMessageDialog(null, "La lampara está " + lampara1.estadoLampara());
+
+        lampara1.cambiarEstado();
+
+        JOptionPane.showMessageDialog(null, "La lampara ahora está " + lampara1.estadoLampara());
+
+    }
+
+
+    
+}
